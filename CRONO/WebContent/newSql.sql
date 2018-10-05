6e2a681841155d357c408b4a74a4f264ca76d068
Joshua Yang <at13josh@gmail.com>
2018-10-05 20:30:38 +1000

    SQL

--- CRONO/WebContent/newSql.sql
+++ CRONO/WebContent/newSql.sql
@@ -27,19 +27,19 @@ INSERT INTO APP.user VALUES (006, 'David', 'Run','email7@example.com','user5', '
 --
 --
 CREATE TABLE APP.user_roles(
+	userID INT,
 	roleID INT,
 	role VARCHAR(50),
-	PRIMARY KEY (roleID)
-	FOREIGN KEY (userID) REFERENCES APP.user(userID)
-	FOREIGN KEY (permID) REFERENCES APP.roles_permissio(permID));
+	PRIMARY KEY (userroleID)
+	FOREIGN KEY (userID) REFERENCES APP.user(userID));
 --
-INSERT INTO APP.user_roles VALUES (000, 'Admin');
-INSERT INTO APP.user_roles VALUES (001, 'Admin');
-INSERT INTO APP.user_roles VALUES (002, 'Employee');
-INSERT INTO APP.user_roles VALUES (003, 'Employee');
-INSERT INTO APP.user_roles VALUES (004, 'Employee');
-INSERT INTO APP.user_roles VALUES (005, 'Employee');
-INSERT INTO APP.user_roles VALUES (006, 'Employee');
+INSERT INTO APP.user_roles VALUES (001, 0, 'Admin');
+INSERT INTO APP.user_roles VALUES (002, 0, 'Admin');
+INSERT INTO APP.user_roles VALUES (003, 1, 'Employee');
+INSERT INTO APP.user_roles VALUES (004, 1, 'Employee');
+INSERT INTO APP.user_roles VALUES (005, 1, 'Employee');
+INSERT INTO APP.user_roles VALUES (006, 1, 'Employee');
+INSERT INTO APP.user_roles VALUES (007, 1, 'Employee');
 --
 --
 CREATE TABLE APP.roles_permission(
@@ -47,6 +47,16 @@ CREATE TABLE APP.roles_permission(
 	role VARCHAR(50),
 	permission VARCHAR(50)
 	PRIMARY KEY (PermID));
+	
+INSERT INTO APP.roles_permission(1, 'Admin', 'admin:edit_time')
+INSERT INTO APP.roles_permission(2, 'Admin', 'admin:login')
+INSERT INTO APP.roles_permission(3, 'Admin', 'admin:create_user')
+INSERT INTO APP.roles_permission(4, 'Admin', 'admin:edit_user')
+INSERT INTO APP.roles_permission(5, 'Admin', 'admin:view_time')
+INSERT INTO APP.roles_permission(6, 'Admin', 'admin:pay')
+INSERT INTO APP.roles_permission(7, 'Employee', 'employee:add_time')
+INSERT INTO APP.roles_permission(8, 'Employee', 'employee:view_time')
+INSERT INTO APP.roles_permission(9, 'Employee', 'employee:edit_profile')
 
 CREATE TABLE APP.times(
    userID INT,
@@ -57,9 +67,9 @@ CREATE TABLE APP.times(
    FOREIGN KEY (userID) REFERENCES APP.employees(userID),
    PRIMARY KEY (timeID));
  
-INSERT INTO APP.times VALUES (001,01, '09:23', '17:30','11/11/2011');
-INSERT INTO APP.times VALUES (002,02, '09:24', '17:31','12/11/2011');
-INSERT INTO APP.times VALUES (003,03, '09:25', '17:32','13/11/2011');
+INSERT INTO APP.times VALUES (001,1, '09:23', '17:30','11/11/2011');
+INSERT INTO APP.times VALUES (002,2, '09:24', '17:31','12/11/2011');
+INSERT INTO APP.times VALUES (003,3, '09:25', '17:32','13/11/2011');
 INSERT INTO APP.times VALUES (004,4, '09:26', '17:33','14/11/2011');
 INSERT INTO APP.times VALUES (005,5, '09:27', '17:34','15/11/2011');
 --
@@ -86,12 +96,3 @@ INSERT INTO APP.pay VALUES(4, 25.5);
 INSERT INTO APP.pay VALUES(5, 25.5);
 INSERT INTO APP.pay VALUES(6, 25.5);
 
-
-
-	
-INSERT INTO APP.roles VALUES ("Admin","");
-INSERT INTO APP.pay VALUES (2, 25.5);
-INSERT INTO APP.pay VALUES(3, 25.5);
-INSERT INTO APP.pay VALUES(4, 25.5);
-INSERT INTO APP.pay VALUES(5, 25.5);
-INSERT INTO APP.pay VALUES(6, 25.5);
